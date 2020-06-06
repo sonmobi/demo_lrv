d3f0b9472d88d89a5f6244ca441d9574f61cfb8f
sonmobi <sonmobi@gmail.com>
2020-06-06 16:13:28 +0700

    Buoc 11 - Chan quyen truy cap https://saophaixoan.net/threads/thuc-hanh-laravel-framework-11-chan-quyen-muc-1-yeu-cau-dang-nhap-moi-duoc-vao-action.77/

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,20 @@ Route::get('/', function () {
 
 
 Route::get('/user','UserController@Index')->name('User.Index');
-Route::match(['get','post'],'/user/add','UserController@Add') ->name('User.Add');
+
+Route::match(['get','post'],'/user/add','UserController@Add')
+    ->name('User.Add')
+    ->middleware('auth');
+
 Route::match(['get','post'],'/user/edit/{id}','UserController@Edit')
     ->where(['id'=>'[0-9]+'])
-    ->name('User.Edit');
+    ->name('User.Edit')
+    ->middleware('auth');
+
 Route::match(['get','post'],'/user/delete/{id}','UserController@Delete')
     ->where(['id'=>'[0-9]+'])
-    ->name('User.Delete');
+    ->name('User.Delete')
+    ->middleware('auth');
 
 
 Route::match(['get','post'],'/upload','AdminController@Upload') ->name('User.Upload');
